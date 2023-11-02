32f88c8de0721aa9a25abec9150df65aa96580f1
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2023-11-01 23:01:23 -0300

    Initial commit

---
+++ src/main/java/com/example/learnSpring/LearnSpringApplication.java
@@ -0,0 +1,15 @@
+package com.example.learnSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class LearnSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringApplication.class, args);
+	}
+
+}
